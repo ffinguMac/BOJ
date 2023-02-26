efb810daa1a365b1483a5a75c28ad9d57dd52cd6
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-02-26 23:29:55 +0900

    Create 2851.cpp

---
+++ 2851.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int mushroom[10];
+	for (int i = 0; i < 10; i++)
+		cin >> mushroom[i];
+	int sum = 0, res = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		res = sum + mushroom[i];
+		if (res >= 100)
+		{
+			if (res - 100 <= 100 - sum)
+				cout << res;
+			else
+				cout << sum;
+			return 0;
+		}
+		sum = res;
+	}
+	cout << sum;
+
+	return 0;
+}
