aee857363ee3697fdfd8d84ffe32b350e92930ec
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-04 22:34:54 +0900

    Update 2460.cpp

--- 2460.cpp
+++ 2460.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main(void)
 {
-	int a, b;
-	int person = 0, max_value = -1;
+  int a, b;
+  int person = 0, max_value = -1;
 
-	for (int i = 0; i < 10; i++)
-	{
-		cin >> a >> b;
-		person += -a + b;
-		if (person > max_value)
-			max_value = person;
-	}
-	cout << max_value;
+  for (int i = 0; i < 10; i++)
+  {
+    cin >> a >> b;
+    person += -a + b;
+    if (person > max_value)
+      max_value = person;
+  }
+  cout << max_value;
 
-	return 0;
+  return 0;
 }
