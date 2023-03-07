9e4997a50310d425fadc97d1f04d463697006065
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-07 23:47:37 +0900

    Update 1110.cpp

--- 1110.cpp
+++ 1110.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main(void)
 {
-	int N, cnt = 0, tmp;
-	cin >> N;
-	tmp = N;
-	while (true)
-	{
-		tmp = (tmp % 10) * 10 + (tmp / 10 + tmp % 10) % 10;
-		cnt++;
-		if (tmp == N)
-			break;
-	}
-	cout << cnt;
+  int N, cnt = 0, tmp;
+  cin >> N;
+  tmp = N;
+  while (true)
+  {
+    tmp = (tmp % 10) * 10 + (tmp / 10 + tmp % 10) % 10;
+    cnt++;
+    if (tmp == N)
+      break;
+  }
+  cout << cnt;
 
-	return 0;
+  return 0;
 }
