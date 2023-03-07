d114d3f55deb45575d19f71e1b52847f4fed7709
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-07 23:45:49 +0900

    Update 11050.cpp

--- 11050.cpp
+++ 11050.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 int iterative_factorial(int n)
 {
-	int res = 1;
-	for (int i = 1; i <= n; i++)
-		res *= i;
+  int res = 1;
+  for (int i = 1; i <= n; i++)
+    res *= i;
 
-	return res;
+  return res;
 }
+
 int recursive_factorial(int n)
 {
-	if (n == 1 || n == 0)
-		return 1;
-	else
-		return recursive_factorial(n - 1) * n;
+  if (n == 1 || n == 0)
+    return 1;
+  else
+    return recursive_factorial(n - 1) * n;
 }
+
 int main(void)
 {
-	int N, K;
-	cin >> N >> K;
+  int N, K;
+  cin >> N >> K;
+  
+  int res = iterative_factorial(N) / (recursive_factorial(K) * recursive_factorial(N - K));
+  cout << res;
 
-	int res = iterative_factorial(N) / (recursive_factorial(K) * recursive_factorial(N - K));
-	cout << res;
-
-	return 0;
+  return 0;
 }
