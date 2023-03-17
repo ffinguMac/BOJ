6f4a44727ea8b11447f6b07a40f897c7ce2b09f8
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-17 23:04:06 +0900

    Update 1550.cpp

--- 1550.cpp
+++ 1550.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main(void)
 {
-	int res = 0;
-	string arr;
-	cin >> arr;
+  int res = 0;
+  string arr;
+  cin >> arr;
 
-	for (int i = 0; i < arr.size(); i++)
-	{
-		if ('A' <= arr[i] && arr[i] <= 'F')
-			res = res * 16 + arr[i] - 'A' + 10;
-		else
-			res = res * 16 + arr[i] - '0';
-	}
-	cout << res;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if ('A' <= arr[i] && arr[i] <= 'F')
+      res = res * 16 + arr[i] - 'A' + 10;
+    else
+      res = res * 16 + arr[i] - '0';
+  }
+  cout << res;
 
-	return 0;
+  return 0;
 }
