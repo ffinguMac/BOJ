29caf29f01481a63422242ffdcc97e3d5a38fdd7
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-17 23:05:06 +0900

    Update 2851.cpp

--- 2851.cpp
+++ 2851.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main(void)
 {
-	int mushroom[10];
-	for (int i = 0; i < 10; i++)
-		cin >> mushroom[i];
-	int sum = 0, res = 0;
-	for (int i = 0; i < 10; i++)
-	{
-		res = sum + mushroom[i];
-		if (res >= 100)
-		{
-			if (res - 100 <= 100 - sum)
-				cout << res;
-			else
-				cout << sum;
-			return 0;
-		}
-		sum = res;
-	}
-	cout << sum;
+  int mushroom[10];
+  for (int i = 0; i < 10; i++)
+    cin >> mushroom[i];
+  int sum = 0, res = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    res = sum + mushroom[i];
+    if (res >= 100)
+    {
+      if (res - 100 <= 100 - sum)
+        cout << res;
+      else
+        cout << sum;
+      return 0;
+    }
+    sum = res;
+  }
+  cout << sum;
 
-	return 0;
+  return 0;
 }
