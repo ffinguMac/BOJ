b8b34e850f4c1d64f1aec708c1f11a1a8a3b3d01
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-21 12:48:59 +0900

    Update 분수찾기.cc

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -3,19 +3,19 @@ using namespace std;
 
 int main(void)
 {
-	int X, num = 1;
-	cin >> X;
+  int X, num = 1;
+  cin >> X;
 
-	while (X > num)
-	{
-		X -= num;
-		num++;
-	}
+  while (X > num)
+  {
+    X -= num;
+    num++;
+  }
 
-	if (num % 2 == 1)
-		cout << num + 1 - X << '/' << X;
-	else
-		cout << X << '/' << num + 1 - X;
+  if (num % 2 == 1)
+    cout << num + 1 - X << '/' << X;
+  else
+    cout << X << '/' << num + 1 - X;
 
-	return 0;
+  return 0;
 }
