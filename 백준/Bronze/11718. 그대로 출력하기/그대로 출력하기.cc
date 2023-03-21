a70858744675bb34f7c795ce501d7671e659e80d
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-21 12:42:05 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+  string str;
+  while (true)
+  {
+    getline(cin, str);
+    if (str == "")
+      break;
+    cout << str << '\n';
+  }
+
+  return 0;
+}
