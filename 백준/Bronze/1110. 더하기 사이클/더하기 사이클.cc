764e5bef0cd007395e8a54fc676dd0f0ce23cc79
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-21 12:35:39 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int N, cnt = 0, tmp;
+	cin >> N;
+	tmp = N;
+	while (true)
+	{
+		tmp = (tmp % 10) * 10 + (tmp / 10 + tmp % 10) % 10;
+		cnt++;
+		if (tmp == N)
+			break;
+	}
+	cout << cnt;
+
+	return 0;
+}
