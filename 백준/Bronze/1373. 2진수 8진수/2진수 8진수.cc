59fdbf4b2b6092d5e2b89cc8c90bd8165228fe53
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-21 13:06:04 +0900

    [Bronze I] Title: 2진수 8진수, Time: 52 ms, Memory: 4932 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string nb;
+	cin >> nb;
+	int i = 0;
+	while (nb.length() % 3 != 0)
+		nb = '0' + nb;
+	for (int j = 0; j < nb.length(); j += 3)
+	{
+		int res = (nb[j] - '0') * 4 + (nb[j + 1] - '0') * 2 + (nb[j + 2] - '0') * 1;
+		cout << res;
+	}
+
+	return 0;
+}
