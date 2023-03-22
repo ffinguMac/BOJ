55f81c240799aec31080a973b550ada36e5cf335
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-03-22 23:51:35 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	int res = 0;
+	string arr;
+	cin >> arr;
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if ('A' <= arr[i] && arr[i] <= 'F')
+			res = res * 16 + arr[i] - 'A' + 10;
+		else
+			res = res * 16 + arr[i] - '0';
+	}
+	cout << res;
+
+	return 0;
+}
