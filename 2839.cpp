dfbe1a8191535d5bf464df8425cf45ba963834c8
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-04-21 23:13:58 +0900

    Update 2839.cpp

--- 2839.cpp
+++ 2839.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    int five_cnt, three_cnt;
-    five_cnt = N / 5;
-    while (1)
+  int N;
+  cin >> N;
+  int five_cnt, three_cnt;
+  five_cnt = N / 5;
+  while (1)
+  {
+    if (five_cnt < 0)
     {
-        if (five_cnt < 0)
-        {
-            cout << "-1";
-            return 0;
-        }
-        if ((N - (5 * five_cnt)) % 3 == 0)
-        {
-            three_cnt = (N - (5 * five_cnt)) / 3;
-            break;
-        }
-        five_cnt--;
+        cout << "-1";
+        return 0;
     }
+    if ((N - (5 * five_cnt)) % 3 == 0)
+    {
+      three_cnt = (N - (5 * five_cnt)) / 3;
+      break;
+    }
+    five_cnt--;
+  }
     cout << five_cnt + three_cnt;
     
     return 0;
