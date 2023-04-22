4b8121c9f7f57c7e5d8c4378466c600014898b1f
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-04-23 00:04:23 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -3,19 +3,22 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n ;
-	vector<int> dp(n + 1);
+int main(void)
+{
+  int N;
+  cin >> N;
+  vector <int> dp(N + 1);
 
-	//bottom-up
-	dp[1] = 0;
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] + 1;
-		if (!(i % 3)) dp[i] = min(dp[i],dp[i / 3] + 1);
-		if (!(i % 2)) dp[i] = min(dp[i], dp[i / 2] + 1);
-	}
+  dp[1] = 0;
+  for (int i = 2; i <= N; i++)
+  {
+    dp[i] = dp[i - 1] + 1;
+    if (i % 3 == 0)
+      dp[i] = min(dp[i], dp[i / 3] + 1);
+    if (i % 2 == 0)
+      dp[i] = min(dp[i], dp[i / 2] + 1);
+  }
+  cout << dp[N];
 
-	cout << dp[n] << endl;
-	return 0;
+  return 0;
 }
