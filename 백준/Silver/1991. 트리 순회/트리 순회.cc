751ede050f14d0ff0e6981c4f722cf6d2c406f24
Jaewook <54384059+ffinguMac@users.noreply.github.com>
2023-05-06 20:31:16 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,30 +1,36 @@
 #include <iostream>
 using namespace std;
 
-void preorder(int N, int tree[][2]) {
-  if (N == -1) return;
+int tree[27][2];
+void preorder(int N) 
+{
+  if (N == -1) 
+    return;
   cout << (char)(N + 'A');
-  preorder(tree[N][0], tree);
-  preorder(tree[N][1], tree);
+  preorder(tree[N][0]);
+  preorder(tree[N][1]);
 }
 
-void inorder(int N, int tree[][2]) {
-  if (N == -1) return;
-  inorder(tree[N][0], tree);
+void inorder(int N) 
+{
+  if (N == -1) 
+    return;
+  inorder(tree[N][0]);
   cout << (char)(N + 'A');
-  inorder(tree[N][1], tree);
+  inorder(tree[N][1]);
 }
 
-void postorder(int N, int tree[][2]) {
-  if (N == -1) return;
-  postorder(tree[N][0], tree);
-  postorder(tree[N][1], tree);
+void postorder(int N) 
+{
+  if (N == -1) 
+    return;
+  postorder(tree[N][0]);
+  postorder(tree[N][1]);
   cout << (char)(N + 'A');
 }
 int main(void)
 {
   int N;
-  int tree[27][2];
   cin >> N;
   for (int i = 0; i < N; i++)
   {
@@ -40,11 +46,11 @@ int main(void)
     else
       tree[a][1] = c - 'A';
   }
-  preorder(0, tree);
+  preorder(0);
   cout << '\n';
-  inorder(0, tree);
+  inorder(0);
   cout << '\n';
-  postorder(0, tree);
+  postorder(0);
 
   return 0;
 }
